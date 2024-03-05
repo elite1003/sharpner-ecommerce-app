2429885ff2464418caa4aafdd396c80cacf65078
elite1003 <sudhirkumar10397@gmail.com>
2024-03-05 17:13:53 +0530

    add Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { ListGroup, Button } from "react-bootstrap";
 
 const Home = () => {
-  return <div>Home</div>;
+  const details = [
+    {
+      date: new Date().toLocaleString(),
+      place: "Lucknow",
+      description: "Lorem epsum dimdaba dininsin ",
+    },
+    {
+      date: new Date().toLocaleString(),
+      place: "Lucknow",
+      description: "Lorem epsum dimdaba dininsin ",
+    },
+    {
+      date: new Date().toLocaleString(),
+      place: "Lucknow",
+      description: "Lorem epsum dimdaba dininsin ",
+    },
+    {
+      date: new Date().toLocaleString(),
+      place: "Lucknow",
+      description: "Lorem epsum dimdaba dininsin ",
+    },
+    {
+      date: new Date().toLocaleString(),
+      place: "Lucknow",
+      description: "Lorem epsum dimdaba dininsin ",
+    },
+  ];
+  const detailList = details.map((item) => (
+    <ListGroup.Item>
+      <div className="d-flex w-100 justify-content-between">
+        <h5>{item.date}</h5>
+        <small>{item.place}</small>
+        <p>{item.description} </p>
+        <Button variant="primary"> Buy Ticket</Button>
+      </div>
+    </ListGroup.Item>
+  ));
+  return <ListGroup>{detailList}</ListGroup>;
 };
 
 export default Home;
